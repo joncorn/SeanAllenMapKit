a86f583f4ed4bec86c8bfb94fc11f0a75df84592
Jon <jdcorn@live.com>
2020-04-05 14:37:44 -0600

    add project files

---
+++ SeanAllenMapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeanAllenMapKit
+//
+//  Created by Jon Corn on 4/5/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
